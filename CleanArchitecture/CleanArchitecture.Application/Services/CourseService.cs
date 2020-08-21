139eda42cf9eb56ab9715f48e772c2cac90d9535
stefan.sadovek <stefan.sadovek@fincore.com>
2020-08-21 17:56:00 +0200

    Added Application project and Course service

---
+++ CleanArchitecture/CleanArchitecture.Application/Services/CourseService.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModels;
+using System.Collections.Generic;
+
+namespace CleanArchitecture.Application.Services
+{
+	public class CourseService : ICourseService
+	{
+		public IEnumerable<CourseViewModel> GetCourses()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
