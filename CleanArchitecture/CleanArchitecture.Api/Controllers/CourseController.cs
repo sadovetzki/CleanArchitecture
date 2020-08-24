8aad723d28ce2f692d0dde8f61f1211a3ccb35c8
stefan.sadovek <stefan.sadovek@fincore.com>
2020-08-24 12:39:19 +0200

    Added CleanArchitecture.Api project

---
+++ CleanArchitecture/CleanArchitecture.Api/Controllers/CourseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Api.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class CourseController : ControllerBase
+	{
+	}
+}
