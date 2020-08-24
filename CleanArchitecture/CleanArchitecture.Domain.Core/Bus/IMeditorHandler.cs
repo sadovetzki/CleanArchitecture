ef984e68a3ed35baa12552ad8eb4ffff2b12ab4a
stefan.sadovek <stefan.sadovek@fincore.com>
2020-08-24 12:08:49 +0200

    Added Domain.Core and Implementation of InMemoryBus

---
+++ CleanArchitecture/CleanArchitecture.Domain.Core/Bus/IMeditorHandler.cs
@@ -0,0 +1,10 @@
+﻿using CleanArchitecture.Domain.Core.Commands;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Core.Bus
+{
+	public interface IMeditorHandler
+	{
+		Task SendCommand<T>(T command) where T : Command;
+	}
+}
--- CleanArchitecture/CleanArchitecture.Infra.Bus/InMemoryBus.cs
+++ CleanArchitecture/CleanArchitecture.Infra.Bus/InMemoryBus.cs
@@ -1,11 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using CleanArchitecture.Domain.Core.Bus;
+using CleanArchitecture.Domain.Core.Commands;
+using MediatR;
+using System.Threading.Tasks;
 
 namespace CleanArchitecture.Infra.Bus
 {
-	public class InMemoryBus
+	public sealed class InMemoryBus : IMeditorHandler
 	{
+		private readonly IMediator mediator;
 
+		public InMemoryBus(IMediator mediator)
+		{
+			this.mediator = mediator;
+		}
+
+		public Task SendCommand<T>(T command) where T : Command
+		{
+			return mediator.Send(command);
+		}
 	}
 }
